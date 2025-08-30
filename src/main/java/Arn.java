2bdb454834d341b7a3b7587a4cd8cfc5137f3834
Aryann-Sharma <e1295594@u.nus.edu>
2025-08-30 15:47:20 +0800

    Add functionality to echo user commands and exit for bye command

--- src/main/java/Arn.java
+++ src/main/java/Arn.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Arn {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Arn");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while(true) {
+            System.out.print("-> ");
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+                System.out.print("\n");
+            }
+        }
+
+        scanner.close();
     }
 }
